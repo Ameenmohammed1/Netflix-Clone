81575c0ef019e3cdfffdd1a07be4a7ae9b6115e3
Ameen mohammed <turtlecompany9@gmail.com>
2023-05-22 02:27:32 -0400

    Updates

---
+++ precache-manifest.e1c14e64a09b493dd0737a0249d68c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93e6569cc0acf4e11bde",
+    "url": "/Netflix-Clone/static/css/main.fe188c6a.chunk.css"
+  },
+  {
+    "revision": "93e6569cc0acf4e11bde",
+    "url": "/Netflix-Clone/static/js/main.93e6569c.chunk.js"
+  },
+  {
+    "revision": "27da8fd49ccaeae2a801",
+    "url": "/Netflix-Clone/static/js/1.27da8fd4.chunk.js"
+  },
+  {
+    "revision": "6f1b2192e8243659d74a",
+    "url": "/Netflix-Clone/static/js/runtime~main.6f1b2192.js"
+  },
+  {
+    "revision": "721c8c41a0ef55c514195e8664c2ef4f",
+    "url": "/Netflix-Clone/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Netflix-Clone/precache-manifest.e1c14e64a09b493dd0737a0249d68c68.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Netflix-Clone/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
